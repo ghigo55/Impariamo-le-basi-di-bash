0cee1b378e2d28d536c565e6f9dc0d8994288f2d
Gabriele <51490573+ghigo55@users.noreply.github.com>
2024-02-12 00:01:55 +0000

    Stampa un messaggio di benvenuto
    Crea uno script che stampi un messaggio di benvenuto quando viene eseguito.

---
+++ esercizio1.sh
@@ -0,0 +1,12 @@
+#Stampa un messaggio di benvenuto: Crea uno script che stampi un messaggio di benvenuto quando viene eseguito.
+
+
+#!/bin/bash
+
+# Questo è un commento. Le righe che iniziano con "#" sono commenti e non vengono eseguite.
+
+# Stampa un messaggio di benvenuto
+echo "Benvenuto! Questo è uno script in Bash che stampa un messaggio di benvenuto."
+
+# Fine dello script
+
